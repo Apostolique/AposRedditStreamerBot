6d980df851746373bfafab784fec5ee9437f1a09
Jean-David Moisan <vynecompiler@gmail.com>
2017-07-17 18:23:04 -0400

    Online / Offline messages

--- AposStreamerRules.py
+++ AposStreamerRules.py
@@ -9,3 +9,9 @@ gameName = "League of Legends"
 #A word that needs to be in the streamer's title. This needs to be in lower case.
 #Streamers that don't have that word are excluded.
 textInTitle = "sion"
+
+#When there are streamers, this will be displayed.
+onlineMessage = "* Online Sion streams!"
+
+#When no one is streaming, display this message instead.
+offlineMessage = "* No one is streaming Sion right now!\n* If you are a Sion streamer, talk to Apostolique and put 'Sion' in your stream title."
