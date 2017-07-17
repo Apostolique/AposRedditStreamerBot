0c658b321933f59ba24f227ea11ebbe5c867a656
Jean-David Moisan <vynecompiler@gmail.com>
2017-07-17 18:03:37 -0400

    First settings commit
    Fill in your info and off you go!

---
+++ AposRedditBotSettings.py
@@ -0,0 +1,14 @@
+''' USER CONFIG '''
+
+# https://www.reddit.com/comments/3cm1p8/how_to_make_your_bot_use_oauth2/
+APP_ID = ""
+APP_SECRET = ""
+APP_URI = "http://127.0.0.1:65010/authorize_callback"
+APP_REFRESH = ""
+
+# This is a short description of what the bot does. 
+# For example "A bot that pulls Twitch streamers for /r/DirtySionMains. (v0.01 by /u/apostolique)"
+USERAGENT = "A bot that pulls Twitch streamers for /r/DirtySionMains. (v0.01 by /u/apostolique)"
+SUBREDDIT = "DirtySionMains"
+
+PASSWORD="" #From http://twitchapps.com/tmi/ without oauth:
